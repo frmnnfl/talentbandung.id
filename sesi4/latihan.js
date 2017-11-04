d679a57b31b06215f1175f83bb7b641999a55acb
frmnnfl <firman.na99@gmail.com>
2017-11-04 15:48:56 +0700

    Belajar JavaScript

---
+++ sesi4/latihan.js
@@ -0,0 +1,252 @@
+/*
+var namaBuah= ["Anggur", "Naga", "Jambu", "Orange" , "Mangga"];
+
+console.log (namaBuah[0]);
+
+namaBuah[4] = "Mangga Manis";
+
+var person={
+  name    : 'Firman Naufal',
+  umur    : '18',
+  address : 'Disana',
+  gadgets:{
+    laptop : 'MBP',
+    phone  : 'S6'
+  },
+  hobby:['tidur', 'tidur lagi']
+};
+
+console.log ( person.name);
+console.log ( person.gadgets.laptop);
+console.log ( person.hobby[1]);
+
+*/
+
+
+// var quran={
+//   "code": 200,
+//   "status": "OK",
+//   "data": {
+//     "number": 1,
+//     "name": "سورة الفاتحة",
+//     "englishName": "Al-Faatiha",
+//     "englishNameTranslation": "The Opening",
+//     "revelationType": "Meccan",
+//     "numberOfAyahs": 7,
+//     "ayahs": [
+//           {
+//             "number": 1,
+//             "text": "﻿بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ",
+//             "numberInSurah": 1,
+//             "juz": 1,
+//             "manzil": 1,
+//             "page": 1,
+//             "ruku": 1,
+//             "hizbQuarter": 1,
+//             "sajda": false
+//           },
+//           {
+//             "number": 2,
+//             "text": "الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ",
+//             "numberInSurah": 2,
+//             "juz": 1,
+//             "manzil": 1,
+//             "page": 1,
+//             "ruku": 1,
+//             "hizbQuarter": 1,
+//             "sajda": false
+//           },
+//           {
+//             "number": 3,
+//             "text": "الرَّحْمَٰنِ الرَّحِيمِ",
+//             "numberInSurah": 3,
+//             "juz": 1,
+//             "manzil": 1,
+//             "page": 1,
+//             "ruku": 1,
+//             "hizbQuarter": 1,
+//             "sajda": false
+//           },
+//           {
+//             "number": 4,
+//             "text": "مَالِكِ يَوْمِ الدِّينِ",
+//             "numberInSurah": 4,
+//             "juz": 1,
+//             "manzil": 1,
+//             "page": 1,
+//             "ruku": 1,
+//             "hizbQuarter": 1,
+//             "sajda": false
+//           },
+//           {
+//             "number": 5,
+//             "text": "إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ",
+//             "numberInSurah": 5,
+//             "juz": 1,
+//             "manzil": 1,
+//             "page": 1,
+//             "ruku": 1,
+//             "hizbQuarter": 1,
+//             "sajda": false
+//           },
+//           {
+//             "number": 6,
+//             "text": "اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ",
+//             "numberInSurah": 6,
+//             "juz": 1,
+//             "manzil": 1,
+//             "page": 1,
+//             "ruku": 1,
+//             "hizbQuarter": 1,
+//             "sajda": false
+//           },
+//           {
+//             "number": 7,
+//             "text": "صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّينَ",
+//             "numberInSurah": 7,
+//             "juz": 1,
+//             "manzil": 1,
+//             "page": 1,
+//             "ruku": 1,
+//             "hizbQuarter": 1,
+//             "sajda": false
+//           }
+//         ],
+//         "edition": {
+//           "identifier": "quran-simple",
+//           "language": "ar",
+//           "name": "Simple",
+//           "englishName": "Simple",
+//           "format": "text",
+//           "type": "quran"
+//         }
+//       }
+// }
+// ;
+
+// console.log(quran.data.name);
+// console.log(quran.data.ayahs[0].text);
+// console.log(quran.data.ayahs[1].text);
+// console.log(quran.data.ayahs[2].text);
+// console.log(quran.data.ayahs[3].text);
+// console.log(quran.data.ayahs[4].text);
+// console.log(quran.data.ayahs[5].text);
+// console.log(quran.data.ayahs[6].text);
+
+
+/*
+var nilai=8;
+if (nilai >=9){
+  alert("Selamat anda lulus tjoi")
+} else if (nilai>=5){
+  alert ("Selamat anda mau lulus")
+} else {
+  alert("Coba lagi gan...")
+}
+*/
+
+// var jam = 19;
+// if (jam >=1 && jam <=11) {
+//   alert("Selamat Pagi");
+// }
+// else if(jam >=12 && jam <=15){
+//   alert("Selamat Siang");
+// }
+// else if(jam >=16 && jam <=18){
+//   alert("Selamat Sore");
+// }
+// else if(jam >=19 && jam <=24){
+//   alert("Selamat Malam");
+// }
+// else {
+//   alert("Selamat");
+// }
+
+// var buah="anggur";
+// switch (buah) {
+//   case "jeruk":
+//     alert("warna buah jeruk itu orange/kuning");
+//     break;
+//   case "apel":
+//       alert ("Warna buah apel itu merah");
+//     break;
+//   default:
+//       alert("Tidak mengenali buah");
+// }
+//
+//
+// var hari= 5;
+//  switch (hari) {
+//   case 0:
+//     alert("Hari Senin");
+//     break;
+//   case 1:
+//     alert("Hari Selasa");
+//     break;
+//   case 2:
+//     alert("Hari Rabu");
+//     break;
+//   case 3:
+//     alert("Hari Kamis");
+//     break;
+//   case 4:
+//     alert("Hari Jumat");
+//     break;
+//   case 5:
+//     alert("Hari Sabtu");
+//     break;
+//   case 6:
+//     alert("Hari Minggu");
+//     break;
+//    default:
+//      alert("Tidak Mengenali Hari")
+//  }
+
+// for ( i=1; i<=100; i++){
+//   console.log( 'Perulangan ke-' + i );
+// }
+//
+// var i = 0;
+// while ( i<=100) {
+//   console.log( 'Perulangan ke-' + i );
+//   i++;
+// }
+
+// var buah= ['apel' , 'jeruk' , 'mangga' , 'delima' , 'jambu'];
+//   for (i=0; i<=4; i++){
+//     console.log( buah[i]);
+//   }
+
+// function luasPersegiPanjang( panjang, lebar) {
+// var luas = panjang * lebar;
+// return luas;
+// }
+// contoh penggunanaan
+// luasPersegiPanjang(5, 5);
+
+// function greet(nama){
+//   return alert("Selamat Datang " + nama);
+// }
+// greet('Men');
+
+
+// var btnGreet = document.getElementById('btn')
+//   btnGreet.addEventListener('click', greet);
+// var nama = document.getElementById('nama')
+//   nama.addEventListener('click', nama);
+// var email = document.getElementById('alamat_email')
+//   email.addEventListener('click', email)
+//   //Impelementasi//
+// function greet(){
+//     alert("hello : " + nama.value + ' ' + email.value);
+//   }
+
+function greeting(event){
+  var nama = event.target.textContent;
+  return alert('halo' + nama);
+}
+
+var grid = document.getElementsByClassName('grid');
+for (var i = 0; i < grid.length; i++) {
+  grid[i].addEventListener('click', greeting );
+}
